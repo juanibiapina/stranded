06997307250703907bf6fb8f5be6675f59512643
Juan Ibiapina <juanibiapina@gmail.com>
2017-10-22 10:33:43 +0200

    test: Fix test

--- src/Game.test.js
+++ src/Game.test.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 
 import Game from './Game';
 
+const model = {
+  actions: [],
+  messages: [],
+};
+
+const dispatch = jest.fn();
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
+  ReactDOM.render(<Game model={model} dispatch={dispatch} />, div);
 });
