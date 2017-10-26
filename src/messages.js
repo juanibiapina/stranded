63468ff00a2a9c50b324496fab3ccd51f6270c26
Juan Ibiapina <juanibiapina@gmail.com>
2017-10-26 21:20:56 +0200

    feature: Change text to main player's perspective
    Closes #15.

--- src/messages.js
+++ src/messages.js
@@ -13,13 +13,13 @@ export const messages = {
 };
 
 let messageData = {};
-messageData[messages.STRANDED] = "You're alive. Your space capsule has landed on an unknown planet. The interior of the capsule is dark, but you can see the capsule's hatch indicator light blinking slowly.";
-messageData[messages.CAPSULE_HATCH_STUCK_2] = "You hear a mechanism start moving and dying with a faint sound. Hot smoke fills the interior of the capsule. It's getting hard to breath.";
-messageData[messages.CAPSULE_HATCH_STUCK_1] = "More smoke is released from the hatch. Red emergency lights start blinking.";
-messageData[messages.CAPSULE_MECHANISM_STUCK] = "You hear no sounds from the hatch mechanisms.";
-messageData[messages.CAPSULE_MECHANISM_UNSTUCK] = "You hear a constant, turning noise from within the hatch.";
-messageData[messages.CAPSULE_HATCH_OPEN] = "The hatch slides open. The sunlight is blinding.";
-messageData[messages.NOTHING_HAPPENED] = "Nothing happened.";
+messageData[messages.STRANDED] = "You're alive. Your space capsule has landed on an unknown planet. The only light you can see inside the dark interior of the capsule is a blinking indicator next to the exit hatch cover.";
+messageData[messages.CAPSULE_HATCH_STUCK_2] = "The hatch cover doesn't budge, but you hear machinery moving and dying with a faint sound. You start coughing when hot smoke fills the interior of the capsule.";
+messageData[messages.CAPSULE_HATCH_STUCK_1] = "Your foot hurts from the impact and more smoke is released. Your head starts to feel very light.";
+messageData[messages.CAPSULE_MECHANISM_STUCK] = "You hear nothing.";
+messageData[messages.CAPSULE_MECHANISM_UNSTUCK] = "You hear a constant buzz from near the hatch cover.";
+messageData[messages.CAPSULE_HATCH_OPEN] = "The hatch cover slides open.";
+messageData[messages.NOTHING_HAPPENED] = "Nothing happens.";
 
 export const createMessage = (message) => (
   {
